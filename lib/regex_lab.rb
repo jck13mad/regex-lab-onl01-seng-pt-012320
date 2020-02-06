4e777626ff133eba5b36246066db2540b2179033
Jack Madden <jck13mad@gmail.com>
2020-02-06 18:31:08 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -18,7 +18,7 @@ end
 def first_word_capitalized_and_ends_with_punctuation?(text)
   first_letter = text.scan(/\w+/)[0]
   
-  if (text.scan(/\./)) && (first_letter == first_letter)
+  if (text.scan(/\./)) && (first_letter == first_letter.upcase)
 end
 
 def valid_phone_number?(phone)
